5c67773c7ef3fb35d1682419d4705e7aebc1ec73
luupho <luuchinhphong.hv@gmail.com>
2024-06-28 03:26:04 +0700

    profile admin

---
+++ packages/b2c-admin/pages/my-page/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Layout } from 'antd';
+import Sidebar from '~/components/my-page/Sidebar';
+import ProfileForm from '~/components/my-page/ProfileForm';
+
+const { Sider, Content } = Layout;
+
+const MyPage = () => {
+    const [currentPage, setCurrentPage] = useState('1');
+
+    const renderContent = () => {
+        switch (currentPage) {
+            case '1':
+                return <ProfileForm />;
+            default:
+                return <ProfileForm />;
+        }
+    };
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider className="site-layout-background" width={300}>
+                <Sidebar onMenuClick={setCurrentPage} />
+            </Sider>
+            <Layout style={{ padding: '24px' }}>
+                <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
+                    {renderContent()}
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default MyPage;
